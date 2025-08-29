4b2c5e420042f128fcb28cab86665b0fe9112f59
Paul Guz <pguz@datapa.com>
2025-08-29 16:03:01 +0100

    defaulting tool properties to bools in aichart

--- dist/inmydata-components-all.js
+++ dist/inmydata-components-all.js
@@ -522,18 +522,18 @@
 
         render() {
             const readOnly = this.getAttribute('read-only') || '';
-            const showToolbar = this.getAttribute('show-toolbar') || '';
-            const showTools = this.getAttribute('show-tools') || '';
-            const showToolToggle = this.getAttribute('show-tool-toggle') || '';
+            const showToolbar = this.getAttribute('show-toolbar') || true;
+            const showTools = this.getAttribute('show-tools') || true;
+            const showToolToggle = this.getAttribute('show-tool-toggle') || true;
             const suppressDrilldown = this.getAttribute('suppress-drilldown') || '';
             
             const { render } = this.getCommonAttributes();
 
             const url = this.createUrl("/aichart");
             if (readOnly.length > 0) url.searchParams.append('ReadOnly', readOnly);
-            if (showToolbar.length > 0) url.searchParams.append('ShowToolbar', showToolbar);
-            if (showTools.length > 0) url.searchParams.append('ShowTools', showTools);
-            if (showToolToggle.length > 0) url.searchParams.append('ShowToolToggle', showToolToggle);
+            if (typeof showToolbar === 'boolean' || showToolbar.length > 0) url.searchParams.append('ShowToolbar', showToolbar);
+            if (typeof showTools === 'boolean' || showTools.length > 0) url.searchParams.append('ShowTools', showTools);
+            if (typeof showToolToggle === 'boolean' || showToolToggle.length > 0) url.searchParams.append('ShowToolToggle', showToolToggle);
             if (suppressDrilldown.length > 0) url.searchParams.append('SuppressDrilldown', suppressDrilldown);
 
             if (render) this.renderIframe(url, "inmydata AI Chart");
